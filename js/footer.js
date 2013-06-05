a83e1985cde92c3a0fd3e3a3e3b3d6133a265c20
Linda Zhou <4688253@qq.com>
2013-06-05 10:29:03 +0800

    Adjust the structure of pages. Signed-off-by: Linda Zhou <4688253@qq.com>

---
+++ js/footer.js
@@ -0,0 +1,23 @@
+   
+
+document.write(' <link rel="stylesheet" type="text/css" href="css/footer.css" /> ');
+document.write('  <div class="footer"> ');
+document.write('   <div class="container">  ');
+document.write('     <div class="row"> ');
+document.write('      <div class="span3"> ');
+document.write('        <a href="#"><img src="image/home.png"  /></a> ');
+document.write('        <a href="#"> <img src="image/cart.png"  /></a> ');
+document.write('        <a href="#"><img src="image/user.png"  /></a> ');
+document.write('      </div> ');
+document.write('      <div class="span6" text align="center"> ');
+document.write('       <b> <p >&copy 2013 Company, Inc. & <a href="#">Privacy</a> & <a href="#">Terms</a></p></b> ');
+document.write('      </div> ');
+document.write('      <div class="span2" text align="right">  ');
+document.write('      <p ><b>T : 400-100-12345</b></p> ');
+document.write('     </div> ');
+document.write('     <div class="span1" text align="right"> ');
+document.write('       <a href="#"><img src="image/gototop.png" /></a> ');
+document.write('    </div>  ');
+document.write('   </div>  ');
+document.write('  </div>  ');
+document.write(' </div>   ');
---
+++ js/head.js
@@ -0,0 +1,47 @@
+ 
+document.write(' <link rel="stylesheet" type="text/css" href="../flower/css/head.css" /> ');
+document.write(' <div class="container">');
+       document.write('<div class="masthead" >'); 
+        document.write(' <div class="row">');
+           document.write(' <div class="span2"><a href="index.html"><img src="../flower/image/flowerstore.png" alt=""  /></a></div>');
+           document.write('<div class="span7"></div>');
+           document.write('<a href="index.html"><div class="span1"><img src="../flower/image/iconhome.png"  />Home</div></a>');
+           document.write('<a href="#"><div class="span1"><img src="../flower/image/iconshoppingcart.png"  />Cart</div></a>');
+           document.write('<a href="signin.html" ><div class="span1"> <img src="../flower/image/iconuser.png"  />Sign in </div></a>');
+          document.write('</div>');
+         document.write('<div class="navbar">'); 
+           document.write('<div class="navbar-inner">');
+             document.write('<div class="containernav">');
+             document.write(' <a class="brand">ALL Products List</a>');
+               document.write('<ul class="nav "> ');   
+                  document.write('<li class="active"><a href="#">Home</a></li>');
+                  document.write('<li class="dropdown">');
+                      document.write('<a class="dropdown-toggle" id="dlabel" role="button" data-toggle="dropdown" data-target="flowers.html" href="flowers.html" >Flowers <b class="caret"></b></a>');
+                        <!-- Link or button to toggle dropdown -->
+                          document.write('<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">');
+                             document.write('<li><a tabindex="-1" href="flowers.html">New Arrival</a></li>');
+                             document.write('<li class="disabled"><a tabindex="-1" href="#">Rose</a></li>');
+                             document.write('<li><a tabindex="-1" href="flowers.html">For Desk</a></li>');
+                             document.write('<li class="divider"></li>');
+                             document.write('<li><a tabindex="-1" href="flowers.html">For Wedding</a></li>');
+                          document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li class="dropdown">');
+                       document.write('<a class="dropdown-toggle" id="dlable" role="button" data-toggle="dropdown" data-target="#" href="#">Cakes <b class="caret"></b></a>');
+                          document.write('<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">');
+                             document.write('<li><a tabindex="-1" href="#">Chocolate</a></li>');
+                             document.write('<li class="disabled"><a tabindex="-1" href="#">Mousse</a></li>');
+                             document.write('<li><a tabindex="-1" href="#">Cream</a></li>');
+                             document.write('<li class="divider"></li>');
+                             document.write('<li><a tabindex="-1" href="#">Fruit</a></li>');
+                          document.write('</ul>');
+                   document.write('</li>');
+                   document.write('<li><a href="#">Presents</a></li>');
+                   document.write('<li><a href="#">About</a></li>');
+                   document.write('<li><a href="#">Blog</a></li>');
+                 document.write('</ul>');
+              document.write('</div>');
+            document.write('</div>');
+          document.write('</div>');<!-- /.navbar -->
+        document.write('</div>'); <!-- /.masthead -->
+      document.write('</div>');<!-- /.container -->
